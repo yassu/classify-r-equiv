6c247314e80d345245a032443bc8912d05d638af
yassu <yassu0320.dev@gmail.com>
2018-10-03 22:18:26 +0900

    Add template of task file

---
+++ tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from invoke import task
+
+
+@task
+def update(ctx):
+    """ 入力のデータを作成/更新する """
+
+
+@task
+def learning(ctx):
+    """ 学習する """
+
+
+@task
+def test(ctx):
+    """ テストする """
+
+
+@task
+def run(ctx):
+    """ 学習後, テストする """
