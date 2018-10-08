7f146e097e721ebe973d4a6b52de798e67393ddd
yassu <yassu0320.dev@gmail.com>
2018-10-08 22:04:29 +0900

    Load file

--- classify_r_equiv/learn_and_test.py
+++ classify_r_equiv/learn_and_test.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+from tqdm import tqdm
+import json
+import numpy as np
+
+
+def load_from_json(json_filename):
+    print('Start to load json file')
+    with open(json_filename) as f:
+        json_datas = json.load(f)
+    print('Finish to load json file')
+
+    print('Start to compute data')
+    xs = []
+    ys = np.array([], dtype=np.int64)
+    for json_data in json_datas[:10]:
+        xs.append(np.array(json_data['function_coeffs']))
+        ys = np.append(ys, json_data['function_type'])
+    xs = np.asarray(xs)
+    ys = np.eye(10)[ys.astype(int)]  # 1-of-K 表現に変換
+    return xs, ys
+
+
 def main():
-    print('Learn and test')
+    json_filename = 'assets/input_function_datas.json'
+    train_size=0.8
+    xs, ys = load_from_json(json_filename=json_filename)
