fcd1f6717c133c501246d7137a769240b9b8c60d
yassu <yassu0320.dev@gmail.com>
2018-10-09 01:46:41 +0900

    Revert "Refactoring"
    This reverts commit ae2cb8f323f6626a7d8cfeed1b43812b9bcea990.

--- classify_r_equiv/learn_and_test.py
+++ classify_r_equiv/learn_and_test.py
@@ -90,19 +90,21 @@ def execute(X_train, X_test, Y_train, Y_test):
     # とする
     n_batches = len(X_train)
     for epoch in range(epochs):
+        X_, Y_ = shuffle(X_train, Y_train)
+
         sess.run(train_step, feed_dict={
-            x: X_train,
-            t: Y_train
+            x: X_[:n_batches],
+            t: Y_[:n_batches]
         })
 
         # 訓練データに対する学習の進み具合を出力
         loss = cross_entropy.eval(session=sess, feed_dict={
-            x: X_train,
-            t: Y_train
+            x: X_,
+            t: Y_
         })
         acc = accuracy.eval(session=sess, feed_dict={
-            x: X_train,
-            t: Y_train
+            x: X_,
+            t: Y_
         })
         print('epoch:', f'{epoch}/{epochs}', ' loss:', loss, ' accuracy:', acc)
 
