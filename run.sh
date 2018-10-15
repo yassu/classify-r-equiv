42dbd20166add8449daabce790b15848b2f3a6c6
yassu <yassu0320.dev@gmail.com>
2018-10-16 01:02:48 +0900

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+inv update -d 0 -j assets/input_function_datas-0.json && \
+    echo "Finish 0" && \
+    inv update -d 1 -j assets/input_function_datas-1.json && \
+    echo "Finish 1" && \
+    inv update -d 2 -j assets/input_function_datas-2.json
